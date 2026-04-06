9adffa7c8eb5babc0fcca60f1ba677febfcbd225
ankitbasai <ankitrajputbasai@gmail.com>
2026-04-06 14:56:34 +0530

    Update python.sh

--- python.sh
+++ python.sh
@@ -1,6 +1,6 @@
 #!/bin/basg
 sudo apt-get update
-sudo apt-get install httpd -y
-sudo systemctl start httpd
-sudo systemctl enable httpd
+sudo apt-get install apache2 -y
+sudo systemctl start apache2
+sudo systemctl enable apache2
 
